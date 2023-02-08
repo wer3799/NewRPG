5437419fff910f9d997d55a2e041ff8759940115
ParkYoungJin <>
2023-02-09 05:55:25 +0900

    QuickSheet Init

---
+++ Assets/QuickSheet/Example/Data/Runtime/ExcelExampleData.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+///
+/// !!! Machine generated code !!!
+/// !!! DO NOT CHANGE Tabs to Spaces !!!
+/// 
+[System.Serializable]
+public class ExcelExampleData
+{
+  [SerializeField]
+  int id;
+  public int Id { get {return id; } set { this.id = value;} }
+  
+  [SerializeField]
+  string name;
+  public string Name { get {return name; } set { this.name = value;} }
+  
+  [SerializeField]
+  int strength;
+  public int Strength { get {return strength; } set { this.strength = value;} }
+  
+  [SerializeField]
+  string difficulty;
+  public string Difficulty { get {return difficulty; } set { this.difficulty = value;} }
+  
+  [SerializeField]
+  string text;
+  public string Text { get {return text; } set { this.text = value;} }
+  
+}
