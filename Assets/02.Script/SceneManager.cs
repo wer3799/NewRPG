fb912c360f4a5870edf28cca75b9cbeadea99a07
ParkYoungJin <>
2023-02-27 23:36:37 +0900

    샘플 작업중

--- Assets/02.Script/SceneManager.cs
+++ Assets/02.Script/SceneManager.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+ 
+
 }
--- Assets/InGameCanvas.cs
+++ Assets/InGameCanvas.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoManager : SingletonMono<AutoManager>
+public class InGameCanvas : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
