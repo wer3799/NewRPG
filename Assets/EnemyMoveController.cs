7442efa1684f3fb4f9c220a3de77c739b136c283
ParkYoungJin <>
2023-04-04 02:11:05 +0900

    no message

--- Assets/EnemyMoveController.cs
+++ Assets/EnemyMoveController.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 
 public class EnemyMoveController : MonoBehaviour
 {
+    private static WaitForSeconds setDirectionDelay = new WaitForSeconds(0.1f);
+    
+    public enum EnemyMoveType
+    {
+        Follow
+    }
+    
     private Transform playerTr;
 
     [SerializeField]
     private Rigidbody2D rb;
 
+    [SerializeField]
+    private EnemyMoveType enemyMoveType;
+
+    private EnemyTableData enemyTableData;
+    
 #if UNITY_EDITOR
     private void OnValidate()
     {
@@ -27,8 +39,34 @@ public class EnemyMoveController : MonoBehaviour
         playerTr = PlayerMoveController.Instance.transform;
     }
 
+    private IEnumerator FollowRoutine()
+    {
+        while (true)
+        {
+            Vector2 direction = playerTr.transform.position - this.transform.position;
+            this.rb.velocity = direction.normalized * enemyTableData.Movespeed;
+            yield return setDirectionDelay;
+        }
+        
+    }
+
     public void Initialize(EnemyTableData enemyTableData)
     {
+        this.enemyTableData = enemyTableData;
 
+        StartMoveRoutine();
+    }
+
+    private void StartMoveRoutine()
+    {
+        switch (enemyMoveType)
+        {
+            case EnemyMoveType.Follow:
+                StartCoroutine(FollowRoutine());
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
     }
 }
